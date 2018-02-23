d37543a3451baa67e926e2219c0bf199d584b928
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-02-22 21:18:53 -0800

    add intergration test for adding definition

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -11,12 +11,26 @@ describe('list of words', {:type => :feature}) do
     expect(page).to have_content('Hello')
   end
 end
-describe('add a definition', {:type => :feature}) do
+describe('go to word page', {:type => :feature}) do
   it ('puts') do
     visit('/')
     fill_in('word', :with => 'Chase')
     click_button('add the word')
     click_link('Chase')
-    expect(page).to have_content("Add a definition")
+    expect(page).to have_content('Add a definition')
+  end
+end
+describe('add a definition', {:type => :feature}) do
+  it ('puts') do
+    visit('/')
+    fill_in('word', :with => 'Dogs')
+    click_button('add the word')
+    save_and_open_page
+    click_link('Dogs')
+
+    fill_in('definition', :with => 'hot dog')
+    fill_in('example', :with => 'Chase is a human hot dog')
+    click_button('add the definition')
+    expect(page).to have_content('Chase is a human hot dog')
   end
 end
