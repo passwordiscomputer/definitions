e45d0ebe69fe2c5063a564b0033b90bf52bd1a1f
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-02-22 20:40:34 -0800

    add test for add definition

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -2,17 +2,35 @@ require('rspec')
 require('words')
 require('pry')
 
-describe('dictionary') do
-  it("creates a word and returns a definition using a reader method") do
-    word = Words.new({:word=>"hello", :definition=>"world",:example=>"hello world", :part_of_speech=>"noun"})
-    expect(word.word).to(eq("hello"))
+describe("#words")
+  describe('.new') do
+    it("creates a word and returns a the word using a reader method") do
+      word = Words.new({:word=>"hello", :definition=>"world",:example=>"hello world", :part_of_speech=>"noun"})
+      expect(word.word).to(eq("hello"))
+    end
   end
-   it("adds word to a list of words and returns the definition of a spcific word") do
-     word = Words.new({:word=>"hello", :definition=>"world",:example=>"hello world", :part_of_speech=>"noun"})
-     word.save
-     word2 = Words.new({:word=>"goodbye", :definition=>"universe",:example=>"hello world", :part_of_speech=>"noun"})
-     word.save
-     x = Words.find("hello")
-     expect(Words.find("hello").definition).to(eq("world"))
+  describe('.add_definition') do
+    it("creates a word adds a definition then returns the definition") do
+      word = Words.new({:word=>"hello"})
+      word.add_definition("world")
+      expect(word.definition_list[0]).to(eq("world"))
+    end
   end
-end
+  # describe('.new') do
+  #   it("creates a word and returns a the word using a reader method") do
+  #     word = Words.new({:word=>"hello", :definition=>"world",:example=>"hello world", :part_of_speech=>"noun"})
+  #     expect(word.word).to(eq("hello"))
+  #   end
+  # end
+  # describe('.new') do
+  #   it("creates a word and returns a the word using a reader method") do
+  #     word = Words.new({:word=>"hello", :definition=>"world",:example=>"hello world", :part_of_speech=>"noun"})
+  #     expect(word.word).to(eq("hello"))
+  #   end
+  # end
+  # describe('.new') do
+  #   it("creates a word and returns a the word using a reader method") do
+  #     word = Words.new({:word=>"hello", :definition=>"world",:example=>"hello world", :part_of_speech=>"noun"})
+  #     expect(word.word).to(eq("hello"))
+  #   end
+  # end
